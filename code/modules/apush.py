5807f17153dbbd5cefccb0779854a8a2e0c8a979
Dong Yiguang <dongyg@qq.com>
2020-07-18 16:17:07 -0700

    fix notification

--- code/modules/apush.py
+++ code/modules/apush.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #-*- encoding: utf-8 -*-
 
-import time, hashlib, uuid
+import time, hashlib, uuid, traceback
 import urllib
 import urllib.parse
 import urllib.request
@@ -20,14 +20,13 @@ def caclSignature(secretkey, timestamp, nonce):
 def pushNotification(lid, sid, did, title, content, url):
     timestamp = str(time.time())
     nonce = hashlib.md5(uuid.uuid1().hex.encode('utf8')).hexdigest()
-    signature = caclSignature([sid,lid], timestamp, nonce)
+    signature = caclSignature([sid,did], timestamp, nonce)
     params = {"ts":timestamp, "nc":nonce, "sn":signature, "sync":'', "tl":title, "ct":content, "url":url}
     data = urllib.parse.urlencode(params).encode("utf-8")
-    req = urllib.request.Request("%s/domapi/device/%s/push"%(ATXT_HOST,did), data=data)
+    req = urllib.request.Request("%s/domapi/lic/%s/push"%(ATXT_HOST,lid), data=data)
     if ATXT_HOST.lower().startswith('https'):
         res = urllib.request.urlopen(req, context=context)
     else:
         res = urllib.request.urlopen(req)
     retval = res.read().decode("utf-8")
     return retval
-
